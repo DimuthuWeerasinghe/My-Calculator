0682289e55bfd19cded81dfd05fe9417cbfdeb1f
unknown <weerasinghedimuthu00@gmail.com>
2024-06-19 10:23:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function calc(){
+    let number1= document.getElementById("number1");
+    letnumber1= document.getElementById("number2");
+    let op=document.getElementById("op").Value;
+    let lblOutput= document.getElementById("output");
+    
+    lblOutput.innerHTML= number1+number1;
+}
